8bc80e94d36d3571d440056e4084c7aa2ce0329c
Hebron George <hebrontgeorge@gmail.com>
2014-06-10 17:07:44 -0400

    Starting at the basics, first making all content script injected pages change their background color to red.

--- myscript.js
+++ myscript.js
@@ -1,34 +1,45 @@
-document.addEventListener("keypress", function(e){
-	if (!chrome.runtime)
-	{
-		chrome.runtime = chrome.extension;
-		chrome.runtime.sendMessage({
-		    method: "POST",
-		    action: "xhttp",
-		    url: "http://www.hebrongeorge.com/skl.php",
-		    data: "q=something"
-		}, function(responseText) {
-			console.log(Object.prototype.toString.call(responseText));
-	    	console.log(responseText);
-    		/*Callback function to deal with the response*/
-		});
-	}
-	else if (!chrome.runtime.onMessage)
-	{
-		console.log("Chrome 22-25");
-	}
-	else
-	{
-		chrome.runtime.sendMessage({
-		    method: "POST",
-		    action: "xhttp",
-		    url: "http://www.hebrongeorge.com/skl.php",
-		    data: "q=something"
-		}, function(responseText) {
-			console.log(Object.prototype.toString.call(responseText));
-	    	console.log(responseText);
-    		/*Callback function to deal with the response*/
-		});
-		console.log("Neither");
-	}
-})
+// function onInitFs(fs) {
+// 	  fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+
+//     // fileEntry.isFile === true
+//     // fileEntry.name == 'log.txt'
+//     // fileEntry.fullPath == '/log.txt'
+//     console.log(fileEntry.name);
+//   }, errorHandler);
+// }
+// function errorHandler(e) {
+//   var msg = '';
+
+//   switch (e.code) {
+//     case FileError.QUOTA_EXCEEDED_ERR:
+//       msg = 'QUOTA_EXCEEDED_ERR';
+//       break;
+//     case FileError.NOT_FOUND_ERR:
+//       msg = 'NOT_FOUND_ERR';
+//       break;
+//     case FileError.SECURITY_ERR:
+//       msg = 'SECURITY_ERR';
+//       break;
+//     case FileError.INVALID_MODIFICATION_ERR:
+//       msg = 'INVALID_MODIFICATION_ERR';
+//       break;
+//     case FileError.INVALID_STATE_ERR:
+//       msg = 'INVALID_STATE_ERR';
+//       break;
+//     default:
+//       msg = 'Unknown Error';
+//       break;
+//   };
+
+//   console.log('Error: ' + msg);
+// }
+// window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+// window.webkitStorageInfo.requestQuota(PERSISTENT, 10*1024*1024 /* 10 MB */, function(grantedBytes) {
+//   window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+// }, function(e) {
+//   console.log('Error', e);
+// });
+
+console.log(document.URL);
+//chrome.tabs.executeScript({code: 'document.body.style.backgroundColor="red"'});
+document.body.style.backgroundColor="red";
